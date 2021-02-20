2505f986c4ac51543730b42baebb3856bf20c234
traljohyreabdullah <31196958+traljohyreabdullah@users.noreply.github.com>
2021-02-20 15:02:11 +0800

    Feat: Add client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import './App.css';
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <div className="App">
-      Hello word!
-    
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
